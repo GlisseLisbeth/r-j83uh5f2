197212027f74d351e496a6424aa206df2e7e8524
GlisseLisbethJorge <glisselisbeth@gmail.com>
2021-12-14 19:00:36 -0500

    counter text in textArea

--- src/App.js
+++ src/App.js
@@ -2,11 +2,18 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0,
+      text:""
+    }
+  }
   render() {
     return (
       <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
+        <textarea rows="3" onChange = {(e) =>  this.setState ({counter: e.target.value.length})}></textarea>
+        <div className="counter">{this.state.counter}</div>
       </div>
     );
   }
